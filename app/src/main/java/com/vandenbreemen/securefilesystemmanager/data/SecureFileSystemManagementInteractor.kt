f0ea5dd16b28f31f6642d4d755cda895c289ea22
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2020-09-02 19:48:37 -0400

    Derive external storage directory based on context ext storage directory.

--- app/src/main/java/com/vandenbreemen/securefilesystemmanager/data/SecureFileSystemManagementInteractor.kt
+++ app/src/main/java/com/vandenbreemen/securefilesystemmanager/data/SecureFileSystemManagementInteractor.kt
@@ -1,29 +1,34 @@
 package com.vandenbreemen.securefilesystemmanager.data
 
 import android.content.Context
-import android.os.Environment
+import java.io.File
 
 class SecureFileSystemManagementInteractor(private val secureFileSystemRepository: SecureFileSystemRepository, private val context: Context) {
 
-    private val supportedPaths = listOf(
-        context.getExternalFilesDir(
-            Environment.DIRECTORY_DOCUMENTS
-        ),
-        context.getExternalFilesDir(
-            Environment.DIRECTORY_DOWNLOADS
-        )
-    )
+    private val supportedPaths: List<File> by lazy {
+        val contextPath = context.getExternalFilesDir(null)
+
+        val externalPath = contextPath!!.absolutePath.replace(Regex("Android[/]data[/]${context.packageName}.*"), "")
+
+        listOf(File(externalPath))
+    }
 
     fun getAllSecureFileSystemFiles(): List<String> {
 
         val result = mutableListOf<String>()
         supportedPaths.filterNotNull().forEach { path->
-            result.addAll(secureFileSystemRepository.listSecureFileSystemsIn(
-                path.absolutePath
-            ))
+            addSecureFileSystems(path, result)
         }
 
         return result
+    }
+
+    private fun addSecureFileSystems(inDirectory: File, toList: MutableList<String>) {
+        toList.addAll(secureFileSystemRepository.listSecureFileSystemsIn(
+            inDirectory.absolutePath
+        ))
+
+        inDirectory.listFiles()?.filter { f->f.isDirectory }?.forEach { addSecureFileSystems(it, toList) }
     }
 
 }
