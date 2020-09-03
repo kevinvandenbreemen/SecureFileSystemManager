59d6b47c4402e9128856900570a69e6605db6225
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2020-09-02 20:20:31 -0400

    Start work on content provider to get list of SFS files

---
+++ app/src/main/java/com/vandenbreemen/securefilesystemmanager/SecureFileSystemsProvider.kt
@@ -0,0 +1,78 @@
+package com.vandenbreemen.securefilesystemmanager
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.content.UriMatcher
+import android.database.Cursor
+import android.database.MatrixCursor
+import android.net.Uri
+import com.vandenbreemen.securefilesystemmanager.data.DefaultSecureFileSystemRepository
+import com.vandenbreemen.securefilesystemmanager.data.SecureFileSystemManagementInteractor
+
+class SecureFileSystemsProvider : ContentProvider() {
+
+    companion object {
+        val AUTHORITY = SecureFileSystemsProvider::class.java.canonicalName
+        val SECURE_FILE_SYS_LIST_PATH = "lsSFS"
+        val SECURE_FILE_SYS_LIST_CODE = 1
+    }
+
+    private val secureFileSystemInteractor = SecureFileSystemManagementInteractor(DefaultSecureFileSystemRepository(), context!!)
+
+    private val uriMatcher = UriMatcher(UriMatcher.NO_MATCH)
+    init {
+        uriMatcher.addURI(AUTHORITY, SECURE_FILE_SYS_LIST_PATH, SECURE_FILE_SYS_LIST_CODE)
+    }
+
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int {
+        TODO("Implement this to handle requests to delete one or more rows")
+    }
+
+    override fun getType(uri: Uri): String? {
+
+        if(uriMatcher.match(uri) == SECURE_FILE_SYS_LIST_CODE) {
+            return "text/plain"
+        }
+
+        return null
+    }
+
+    override fun insert(uri: Uri, values: ContentValues?): Uri? {
+        TODO("Implement this to handle requests to insert a new row.")
+    }
+
+    override fun onCreate(): Boolean {
+        TODO("Implement this to initialize your content provider on startup.")
+    }
+
+    override fun query(
+        uri: Uri, projection: Array<String>?, selection: String?,
+        selectionArgs: Array<String>?, sortOrder: String?
+    ): Cursor? {
+        if(uriMatcher.match(uri) == SECURE_FILE_SYS_LIST_CODE) {
+            val allFiles = secureFileSystemInteractor.getAllSecureFileSystemFiles()
+
+            val cursor = MatrixCursor(
+                arrayOf("path")
+            )
+            allFiles.forEach { path->
+                cursor.newRow().add("path", path)
+            }
+
+            context?.apply {
+                cursor.setNotificationUri(contentResolver, uri)
+            }
+
+            return cursor
+        }
+
+        return null
+    }
+
+    override fun update(
+        uri: Uri, values: ContentValues?, selection: String?,
+        selectionArgs: Array<String>?
+    ): Int {
+        TODO("Implement this to handle requests to update one or more rows.")
+    }
+}
